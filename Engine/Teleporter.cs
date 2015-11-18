d92fc1978038d1b06c087bc6f86fb6155c471a9c
StephanieTania <7314072@student.unpar.ac.id>
2015-11-19 00:26:10 +0700

    Penambahan dokumentasi dan percobaan build

--- Engine/Teleporter.cs
+++ Engine/Teleporter.cs
@@ -1,9 +1,15 @@
 namespace Engine
 {
+    /// <summary>
+    /// Kelas abstrak yang merepresentasikan ular atau tangga
+    /// </summary>
     public abstract class Teleporter
     {
         private int head, tail;
 
+        /// <summary>
+        /// Field untuk mengganti atau mengembalikan nilai dari atribut privat head
+        /// </summary>
         public int Head
         {
             get
@@ -16,6 +22,9 @@ namespace Engine
             }
         }
 
+        /// <summary>
+        /// Field untuk mengganti atau mengembalikan nilai dari atribut privat tail
+        /// </summary>
         public int Tail
         {
             get
@@ -28,12 +37,21 @@ namespace Engine
             }
         }
 
+        /// <summary>
+        /// Constructor kelas Teleporter
+        /// </summary>
+        /// <param name="head">Bagian kepala dari objek Teleporter</param>
+        /// <param name="tail">Bagian ekor dari objek Teleporter</param>
         public Teleporter(int head, int tail)
         {
             this.head = head;
             this.tail = tail;
         }
 
+        /// <summary>
+        /// Method abstrak untuk memindahkan pemain
+        /// </summary>
+        /// <param name="p">Objek pemain yang akan dipindahkan</param>
         public abstract void MovePlayer(Player p);
     }
 }
