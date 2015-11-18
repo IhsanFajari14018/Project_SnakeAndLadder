a2d5ef2fc220897d122ce116394779ef0ffa241f
StephanieTania <7314072@student.unpar.ac.id>
2015-11-18 23:17:42 +0700

    Perbaikan untuk penggunaan kelas Random

--- Engine/Board.cs
+++ Engine/Board.cs
@@ -1,3 +1,4 @@
+using System;
 namespace Engine
 {
   class Board
@@ -12,32 +13,38 @@ namespace Engine
       GenerateBoard();
     }
 
-    public void GenerateBoard(){
-      Random r = new Random();
-      int count = 0;
-      int start = 0;
-      int end = 0;
-      while(count<8){
-        start = r.nextInt(98)+1;
-        end = r.nextInt(98)+1;
-        if((start<end)&&IsValidSnakeLadder(tiles[start],tiles[end])){
-        //tiles[start].setSnake(true);
-        tiles[start].SetIsSnake(new Ladder(end,start));
-        tiles[end].SetIsSnake(new Ladder(end,start));
-        count++;
+        public void GenerateBoard()
+        {
+            Random r = new Random();
+            int count = 0;
+            int start = 0;
+            int end = 0;
+            while (count < 8)
+            {
+                start = r.Next(2, 99);
+                end = r.Next(2, 99);
+                if ((start < end) && IsValidSnakeLadder(tiles[start], tiles[end]))
+                {
+                    //tiles[start].setSnake(true);
+                    tiles[start].SetIsSnake(new Ladder(end, start));
+                    tiles[end].SetIsSnake(new Ladder(end, start));
+                    count++;
+                }
+            }
+            count = start = end = 0;
+            while (count < 8)
+            {
+                start = r.Next(2, 99);
+                end = r.Next(2, 99);
+                if ((start > end) && IsValidSnakeLadder(tiles[start], tiles[end]))
+                {
+                    //tiles[start].setSnake(true);
+                    tiles[start].SetIsLadder(new Snake(start, end));
+                    tiles[end].SetIsLadder(new Snake(start, end));
+                    count++;
+                }
+            }
         }
-      }
-      count=start=end=0;
-      while(count<8){
-        start=r.nextInt(98)+1;
-        end=r.nextInt(98)+1;
-        if((start>end)&&IsValidSnakeLadder(tiles[start],tiles[end])){
-          //tiles[start].setSnake(true);
-          tiles[start].SetIsLadder(new Snake(start,end));
-          tiles[end].SetIsLadder(new Snake(start,end));
-          count++;
-      }
-    }
       private bool IsValidSnakeLadder(Tile t1,Tile t2){
         if(t1.GetSnakeLadder()!=null||t2.GetSnakeLadder()!=null){
             return false;
