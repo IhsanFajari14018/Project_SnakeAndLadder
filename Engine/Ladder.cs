af71b8d30ccfb938b4ca9088fe667f43d6610198
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-11-21 10:36:58 +0700

    Menambahkan dokumentasi

--- Engine/Ladder.cs
+++ Engine/Ladder.cs
@@ -2,11 +2,18 @@ namespace Engine
 {
     public class Ladder : Teleporter
     {
+        ///<summary>
+        ///Konstraktor
+        ///</summary>
         public Ladder(int head, int tail) : base(head, tail)
         {
 
         }
 
+        ///<summary>
+        ///Method override untuk memindahkan player
+        ///</summary>
+        ///<param name="p">Pemain yang akan dipindahkan</param>
         public override void MovePlayer(Player p)
         {
             //set posisi pemain pada head
