1fe854dd2ed629c1bb6cd730a7faf3afe659f1aa
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-11-21 10:45:57 +0700

    Menambahkan dokumentasi

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -12,6 +12,12 @@ namespace Engine
 
         private bool win;
 
+        /// <summary>
+        /// Konstraktor
+        /// </summary>
+        /// <param name="position"> posisi pemain</param>
+        /// <param name="name"> nama pemain</param>
+        /// <param name="isComp"> pemain merupakan komputer atau bukan</param>
         public Player(int position, string name, bool isComp)
         {
             this.position = 0;
@@ -20,16 +26,27 @@ namespace Engine
             this.isComp = isComp;
         }
 
+        /// <summary>
+        /// Setter atribut position
+        /// </summary>
+        /// <param name="position"> posisi pemain yang baru</param>
         public void SetPosition(int position)
         {
             this.position = position;
         }
 
+        /// <summary>
+        /// Getter atribut name
+        /// </summary>
         public string GetName()
         {
             return this.name;
         }
 
+        /// <summary>
+        /// Method untuk menjalankan pemain
+        /// </summary>
+        /// <param name="d"> Objek dice</param>
         public void Move(Dice d)
         {
             int steps = d.RollDice();
@@ -43,11 +60,17 @@ namespace Engine
 
         }
 
+        /// <summary>
+        /// Getter atribut position
+        /// </summary>
         public int GetPosition()
         {
             return this.position;
         }
 
+        /// <summary>
+        /// Mengembalikan nilai atribut win
+        /// </summary>
         public bool IsWin()
         {
             if (this.position == 99)
@@ -58,6 +81,9 @@ namespace Engine
             return this.win;
         }
 
+        /// <summary>
+        /// Mengembalikan nilai atribut isComp
+        /// </summary>
         public bool IsCompPlayer()
         {
             return isComp;
