149b254999f742bb5d20b97c58a68368541936ef
StephanieTania <7314072@student.unpar.ac.id>
2015-11-23 13:59:33 +0700

    Perombakan form

--- Project_SnakeAndLadder/MenuForm.cs
+++ Project_SnakeAndLadder/MenuForm.cs
@@ -12,60 +12,24 @@ namespace Project_SnakeAndLadder
 {
     public partial class MenuForm : Form
     {
-        private MainForm form;
+        private RulesSelectionForm form;
         public MenuForm()
         {
             InitializeComponent();
-            form = new MainForm(2);
+            form = new RulesSelectionForm();
+            this.Owner = form;
         }
 
         private void labelPlay_Click(object sender, EventArgs e)
         {
-            labelHuman.Show();
-            labelHuman.Enabled = true;
-            labelComp.Show();
-            labelComp.Enabled = true;
+            this.Hide();
+            form.Show();
         }
 
         private void labelExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
-
-        private void labelHuman_Click(object sender, EventArgs e)
-        {
-            form = new MainForm(1);
-            this.Owner = form;
-            this.Hide();
-            form.Show();
-        }
-
-        private void labelComp_Click(object sender, EventArgs e)
-        {
-            form = new MainForm(2);
-            this.Owner = form;
-            this.Hide();
-            form.Show();
-        }        
-
-        private void labelHuman_MouseLeave(object sender, EventArgs e)
-        {
-            labelHuman.Image = new Bitmap(Properties.Resources.vs_playerButton_beforeClicked);
-        }
-
-        private void labelHuman_MouseMove(object sender, MouseEventArgs e)
-        {
-            labelHuman.Image = new Bitmap(Properties.Resources.vs_playerButton);
-        }
-
-        private void labelComp_MouseLeave(object sender, EventArgs e)
-        {
-            labelComp.Image = new Bitmap(Properties.Resources.vs_computerButton_beforeClicked);
-        }
-
-        private void labelComp_MouseMove(object sender, MouseEventArgs e)
-        {
-            labelComp.Image = new Bitmap(Properties.Resources.vs_computerButton);
-        }
+        
     }
 }
