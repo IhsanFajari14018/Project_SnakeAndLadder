12a596a80dbebc2f7e0ea3dc9791610d29af714c
Lydia Febtriani <7314044@student.unpar.ac.id>
2015-11-23 22:10:13 +0700

    Menambahkan dokumentasi

--- Project_SnakeAndLadder/RulesSelectionForm.cs
+++ Project_SnakeAndLadder/RulesSelectionForm.cs
@@ -10,15 +10,27 @@ using System.Windows.Forms;
 
 namespace Project_SnakeAndLadder
 {
+    /// <summary>
+    /// Kelas ini diturunkan dari kelas Form
+    /// Kelas ini merepresentasikan form yang berisi aturan main
+    /// </summary>
     public partial class RulesSelectionForm : Form
     {
         private MainForm form;
 
+        /// <summary>
+        /// Konstraktor
+        /// </summary>
         public RulesSelectionForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Method bila label human diklik
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelHuman_Click(object sender, EventArgs e)
         {
             form = new MainForm(1);
@@ -27,16 +39,31 @@ namespace Project_SnakeAndLadder
             form.Show();
         }
 
+        /// <summary>
+        /// Method bila mouse dilepaskan dari label human
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelHuman_MouseLeave(object sender, EventArgs e)
         {
             labelHuman.Image = new Bitmap(Properties.Resources.vs_playerButton);
         }
 
+        /// <summary>
+        /// Method bila mouse berpindah dari label human
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelHuman_MouseMove(object sender, MouseEventArgs e)
         {
             labelHuman.Image = new Bitmap(Properties.Resources.vs_playerButton_beforeClicked);
         }
 
+        /// <summary>
+        /// Method bila label comp diklik
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelComp_Click(object sender, EventArgs e)
         {
             form = new MainForm(2);
@@ -45,11 +72,21 @@ namespace Project_SnakeAndLadder
             form.Show();
         }
 
+        /// <summary>
+        /// Method bila mouse dilepaskan dari label comp
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelComp_MouseLeave(object sender, EventArgs e)
         {
             labelComp.Image = new Bitmap(Properties.Resources.vs_computerButton);
         }
 
+        /// <summary>
+        /// Method bila mouse berpindah dari label comp
+        /// </summary>
+        /// <param name="sender">object sender</param>
+        /// <param name="e">event args</param>
         private void labelComp_MouseMove(object sender, MouseEventArgs e)
         {
             labelComp.Image = new Bitmap(Properties.Resources.vs_computerButton_beforeClicked);
